70a1f8ee6f53df5cf2f520d9ee50dca7f71e00a9
Akshay <akshaychandra111@gmail.com>
2019-04-06 19:41:17 +0530

    setup stuff

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(
-  name = 'image_bbox_slicer
+  name = 'image_bbox_slicer',
   packages = ['image_bbox_slicer'],   
   version = '0.1.0',      
   license='MIT',        
